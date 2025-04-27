d03097fa427d6bfcac7faf3d8b38370f8f0804c2
Attila_Tapai <attila.tapai01@gmail.com>
2025-04-27 18:53:02 +0200

    fix category.cs

--- backend/Models/Category.cs
+++ backend/Models/Category.cs
@@ -5,7 +5,7 @@ namespace ExpenseTracker.Models
     public class Category
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public required string Name { get; set; }
 
         public int UserId { get; set; }
 
