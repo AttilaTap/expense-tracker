fae7907ae8f04ca27dd2ef70f1d6f36b1333431c
AttilaTap <attila.tapai01@gmail.com>
2025-04-28 14:03:59 +0200

    removing comments

--- backend.Tests/CategoriesControllerTests.cs
+++ backend.Tests/CategoriesControllerTests.cs
@@ -36,15 +36,12 @@ namespace ExpenseTracker.Tests
         [Test]
         public async Task GetCategories_ReturnsCategories_ForUser()
         {
-            // Arrange
             _context.Categories.Add(new Category { Name = "TestCategory", UserId = 1 });
             await _context.SaveChangesAsync();
 
-            // Act
             var result = await _controller.GetCategories(1);
             var categories = result.Value.ToList();
 
-            // Assert
             Assert.That(categories.Count, Is.EqualTo(1));
             Assert.That(categories[0].Name, Is.EqualTo("TestCategory"));
         }
@@ -52,26 +49,21 @@ namespace ExpenseTracker.Tests
         [Test]
         public async Task CreateCategory_ReturnsBadRequest_WhenCategoryAlreadyExists()
         {
-            // Arrange
             _context.Categories.Add(new Category { Name = "ExistingCategory", UserId = 1 });
             await _context.SaveChangesAsync();
 
             var newCategory = new Category { Name = "ExistingCategory", UserId = 1 };
 
-            // Act
             var result = await _controller.CreateCategory(newCategory);
 
-            // Assert
             Assert.That(result.Result, Is.InstanceOf<BadRequestObjectResult>());
         }
 
         [Test]
         public async Task DeleteCategory_ReturnsNotFound_WhenCategoryDoesNotExist()
         {
-            // Act
             var result = await _controller.DeleteCategory(999);
 
-            // Assert
             Assert.That(result, Is.InstanceOf<NotFoundResult>());
         }
     }
